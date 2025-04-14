55b3a1f863784c1cd10842c2cc68238091d97342
Renato <renatocosta141295@gmail.com>
2025-04-14 13:53:59 +0100

    asdasfa

--- Exercicios_Loop/Ex_TF2.py
+++
@@ -1,6 +0,0 @@
-# Teste Final: Elabore uma base de dados de clientes de uma fábrica de materiais. 
-# O programa deverá possibilitar inserção e listagem dos clientes bem como as compras por eles efetuadas
-# ( Númcli(Automático), NomCli, morada, tel, nif, compra, Divfin ). 
-# Divida final=compra – desconto, valor do desconto se compra for entre 100 e 200 é 5%, se for superior a 200 e inferior a 500 é 10% se superior a 500 é 15%. 
-# O programa deve validar todas as entradas e na listagem deve parar cliente a cliente e ser possível busca direta por número de cliente. 5v. 
-# O programa deve conter (Estruturas 3v, funções .5v, Vetores .4v, apontadores .3v). 2 H 
